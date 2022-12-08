ad7ab1414635636486286eef7019f7348acd3e68
Ap73MKa <artyom.vlasov.05@mail.ru>
2022-12-08 11:42:02 +0300

    ADD: take products from js array, add button in cart

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,71 @@ function close_cart() {
 function unhide_cart() {
     document.getElementById("cart-wrapper").classList.remove("hidden");
 }
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+
+let productList = [
+    {
+        'name': 'Калифорния',
+        'content': 'Ролл',
+        'img': 'assets/california.png',
+        'price': '279.00 ₽'
+    },
+    {
+        'name': 'Филадельфия',
+        'content': 'Ролл',
+        'img': 'assets/philadelphia.png',
+        'price': '359.00 ₽'
+    },
+    {
+        'name': 'Бонито',
+        'content': 'Ролл',
+        'img': 'assets/bonito.png',
+        'price': '299.00 ₽'
+    },
+    {
+        'name': 'Ями',
+        'content': 'Ролл',
+        'img': 'assets/yami.png',
+        'price': '239.00 ₽'
+    },
+]
+
+productList = productList.flatMap(i => Array(5).fill(i));
+shuffle(productList);
+
+let cardItem = ''
+let out = document.getElementById('out')
+
+productList.forEach ((item) => {
+    cardItem +=
+        `
+        <div class="product">
+            <div class="image-block">
+                <img src="${item.img}" alt="">
+            </div>
+            <div class="content shadow border">
+                <div class="text-block">
+                    <p class="title">${item.name}</p>
+                    <p class="info">${item.content}</p>
+                    <div class="price-block">
+                        <p>${item.price}</p>
+                        <a href="#">
+                            <img src="assets/plus.svg" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+})
+out.insertAdjacentHTML('afterbegin', cardItem);
