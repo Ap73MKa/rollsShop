1bc23c045c645b4e81e11d7fc50dea8824ad1219
Ap73MKa <artyom.vlasov.05@mail.ru>
2022-12-21 17:01:58 +0300

    UPDATE: move js, css, assets to static folder, rename folders to default names ADD: .gitignore

---
+++ static/js/swiper.js
@@ -0,0 +1,15 @@
+new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+});
